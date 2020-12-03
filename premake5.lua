2c75c8cc2e22ce3f76eb48d98b5769ef4c7327fd
Nicolas Constanty <nicolas.constanty@outlook.fr>
2020-12-03 04:27:10 +0100

    Load config

---
+++ premake5.lua
@@ -0,0 +1,72 @@
+    -- premake5.lua
+workspace "UdanSandbox"
+
+    architecture "x86_64"
+    startproject "udan_sandbox"
+
+    configurations
+    { 
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+    flags {
+        "MultiProcessorCompile"
+    }
+
+    -- Log library
+    defines { "BUILD_LOGURU", "BUILD_SPDLOG" }
+    -- Graphic library
+    defines { "BUILD_GLFW" }
+
+    defines { "USE_SPDLOG"}
+
+    filter "Debug"
+        defines { "DEBUG" }
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+group "Udan Engine"
+    include "Udan/udan_core"
+    include "Udan/udan_debug"
+    include "Udan/udan_ecs"
+    include "Udan/udan_window"
+    -- Log lib
+    --filter "BUILD_LOGURU"
+      --  include "Udan/udan_loguru"
+    -- filter "BUILD_SPDLOG"
+       --include "Udan/udan_spdlog"
+    -- Graphic lib
+    filter "BUILD_GLFW"
+        include "Udan/udan_glfw"
+
+
+group ""
+
+project "udan_sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    files {
+        "src/**.cpp",
+        "includes/**.h"
+    }
+
+    links { "udan_core", "udan_debug", "udan_ecs", "udan_window" }
+
+    includedirs {
+        "include", 
+        "Udan/udan_core/include/",
+        "Udan/udan_debug/include/",
+        "Udan/udan_ecs/include/",
+        "Udan/udan_window/include/",
+
+        "Udan/ThirdParties/SpdLog/include"
+    }
+
+-- group "ThirdParties"
+--     include "Udan/ThirdParties/GLFW"
